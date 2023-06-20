382fb3ec85ec01664a2a15c193f6ce39e418f856
loumouli <louis.moulin@outlook.fr>
2023-06-20 22:36:36 +0200

    realy bad push ong

--- src/bonus/ft_list_push_front.s
+++ src/bonus/ft_list_push_front.s
@@ -2,17 +2,24 @@ section .text
 	global ft_list_push_front ;export ft_list_push_front
 	extern calloc
 ft_list_push_front: ;void	ft_list_push_front(t_list **beg_list, void *data)
-	test rdi, rdi ; test if rdi is non-null
-	je .end ; jump if null
-	mov r12, rdi ; save the 1rst args inside r12
-	mov r13, rsi ; save the 2nd args inside r13
-	mov rdi, 0x1 ; load value 16 for next calloc call
-	mov rsi, 0x16 ; load value 1 for next calloc call
-	call calloc wrt ..plt ; call calloc, result in rax
-	mov rsi, [r12]
-	mov QWORD [rax+0x8], rsi
-	mov QWORD [rax], r13
+    push rbp
+    mov rbp, rdi
+    mov edi, 0x1
+    push rbx
+    mov rbx, rsi
+    mov esi, 0x10
+    sub rsp, 0x8
+    call calloc wrt ..plt
+    test rax, rax
+    je .end
+    movq   xmm0,rbx
+    movhps xmm0,QWORD [rbp+0x0]
+    mov    QWORD [rbp+0x0],rax
+    movups ga[rax],xmm0
 .end:
+    add    rsp,0x8
+    pop    rbp
+    pop    rbx
 	ret
 
  section .note.GNU-stack noalloc noexec nowrite progbits ;use to silence some warning
--- test.c
+++ test.c
@@ -5,12 +5,17 @@ typedef struct s_list
 	void			*content;
 	struct s_list	*next;
 }					t_list;
+void ft_list_push_front(t_list **begin_list, void *data);
 
-void foo(t_list *list)
-{
-    list->content = "World";
-    list->next = NULL;
-}
+//void ft_list_push_front(t_list **begin_list, void *data)
+//{
+//    t_list *new = calloc(1, sizeof(t_list));
+//    if (!new)
+//        return ;
+//    new->content = data;
+//    new->next = *begin_list;
+//    *begin_list = new;
+//}
 
 int main(void)
 {
@@ -18,7 +23,6 @@ int main(void)
     if (!list)
         return 1;
     list->content = "Hello";
-    extern ft_list_push_front();
     ft_list_push_front(&list, "World");
     printf("%s\n", (char *)list->content);
     printf("%s\n", (char *)list->next->content);
